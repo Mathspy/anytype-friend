8092d0d2849c9c6b73cc68c6e9e4708cea84f7bf
Mathspy <mathspy257@gmail.com>
2024-04-13 15:12:10 -0400

    Create AnyTypeClient with connect method

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,20 @@ mod pb {
 
     tonic::include_proto!("anytype");
 }
+
+use pb::client_commands_client::ClientCommandsClient;
+
+pub struct AnytypeClient {
+    inner: ClientCommandsClient<tonic::transport::Channel>,
+}
+
+impl AnytypeClient {
+    pub async fn connect(url: &str) -> Result<Self, tonic::transport::Error> {
+        use std::str::FromStr;
+
+        let url = tonic::transport::Endpoint::from_str(url)?;
+        Ok(Self {
+            inner: ClientCommandsClient::connect(url).await?,
+        })
+    }
+}
