99f0c7c552121012513d1c60f18a685a227ca57e
Mathspy <mathspy257@gmail.com>
2024-04-16 23:15:38 -0400

    Use a temporary directory in authenticate tests

--- tests/auth.rs
+++ tests/auth.rs
@@ -5,11 +5,14 @@ use utils::run_with_service;
 
 #[tokio::test]
 async fn can_create_an_account_and_authenticate_with_it() {
+    let temp_dir = tempdir::TempDir::new("anytype-friend").unwrap();
+    let temp_dir_path = temp_dir.path();
+
     let (mnemonic, account_id) = run_with_service(|port| async move {
         let (mnemonic, client) = AnytypeClient::connect(&format!("http://127.0.0.1:{port}"))
             .await
             .unwrap()
-            .create_account("Test Client", NetworkSync::LocalOnly)
+            .create_account_with_path("Test Client", NetworkSync::LocalOnly, temp_dir_path)
             .await
             .unwrap();
 
@@ -21,7 +24,7 @@ async fn can_create_an_account_and_authenticate_with_it() {
         let client = AnytypeClient::connect(&format!("http://127.0.0.1:{port}"))
             .await
             .unwrap()
-            .authenticate(&mnemonic)
+            .authenticate_with_path(&mnemonic, temp_dir_path)
             .await
             .unwrap();
 
